29965a46abc358af5a8f8c459194ce1d0ea0c66e
Cathy Lee <leeyu823@gmail.com>
2021-02-22 18:52:41 +0900

    feat: ⚡ sticky mypage header

--- src/layout/profile/index.js
+++ src/layout/profile/index.js
@@ -1,49 +1,70 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Link from 'next/link';
 import styles from './index.module.scss';
 
+/*
+  TODO: mypage
+    - fix header
+    - connect mobx
+    - style
+
+*/
+
 const Avatar = (props) => {
   const img = '';
+  const {imgUrl} = props;
   return (
     <>
       {img 
-        ? <img src={props.imgUrl} className={styles.avatar__img}/>
-        : <div className={styles.avatar__color}></div>
-      }
+        ? <img src={imgUrl} className={styles.avatar__img} alt="user avatar" />
+        : <div className={styles.avatar__color} />}
     </>  
-  )
-}
-const ProfileBody = ({children}) => (
-  <div className={styles.profile_body}>
+  );
+};
+const ProfileBody = ({children, sticky}) => (
+  <div className={[styles.profile_body, sticky && styles.sticky].filter(Boolean).join(' ')}>
     {children}
   </div>
-)
-export default function ProfileLayout(props, {children}) {
+);
+
+export default function ProfileLayout(props) {
+  const {children, comment} = props;
+  const [sticky, setSticky] = useState(false);
   const tabs = ['profile', 'questions', 'answers', 'bookmark'];
   // #0D0D0D
+  const ref = useRef(null);
+  const handleScroll = () => {
+    if (ref.current) {
+      setSticky(ref.current.getBoundingClientRect().bottom <= 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+  }, []);
   return (
-    <>
-      <div className={styles.profile_header}>
+    <div className={styles.container}>
+      <div className={styles.profile_header} ref={ref}>
         <Avatar />
         <p className={styles.profile_header__name}>name</p>
         <p className={styles.profile_header__job}>job</p>
         <p className={styles.profile_header__rank}>rank</p>
         <p className={styles.profile_header__urls}>url icons</p>
         <p className={styles.profile_header__comment}>
-          {props.comment 
-            ? props.comment 
-            : '한 줄 소개'
-          }
+          {comment || '한 줄 소개'}
         </p>
-        <div className={styles.profile_tabs}>
-          {tabs.map((tab, i) => (
-            <Link key={i} href={`/mypage/${tab === 'profile' ? '/' : tab}`}>
-              <a className={styles.profile_tabs__item}>{tab.toLocaleUpperCase()}</a>
-            </Link>
-          ))}
-        </div>
       </div>
-      <ProfileBody>{children}</ProfileBody>
-    </>
+      <div className={[styles.profile_tabs, sticky && styles.sticky].filter(Boolean).join(' ')}>
+        {tabs.map((tab) => (
+          <Link key={tab} href={`/mypage/${tab === 'profile' ? '/' : tab}`}>
+            <span className={styles.profile_tabs__item}>{tab.toLocaleUpperCase()}</span>
+          </Link>
+        ))}
+      </div>
+      <ProfileBody sticky={sticky}>{children}</ProfileBody>
+    </div>
   );
 }
