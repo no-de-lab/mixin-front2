8278d2444d4648c7396578c80606177a3689e0fb
Cathy Lee <leeyu823@gmail.com>
2021-02-24 14:24:08 +0900

    refactor: 🛠 url icons and styled components

--- src/layout/profile/index.js
+++ src/layout/profile/index.js
@@ -1,13 +1,12 @@
 import React, {useState, useRef, useEffect} from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './index.module.scss';
 
 /*
   TODO: mypage
-    - fix header
     - connect mobx
-    - style
-
+    - style refactoring
 */
 
 const Avatar = (props) => {
@@ -31,7 +30,6 @@ export default function ProfileLayout(props) {
   const {children, comment} = props;
   const [sticky, setSticky] = useState(false);
   const tabs = ['profile', 'questions', 'answers', 'bookmark'];
-  // #0D0D0D
   const ref = useRef(null);
   const handleScroll = () => {
     if (ref.current) {
@@ -50,9 +48,21 @@ export default function ProfileLayout(props) {
       <div className={styles.profile_header} ref={ref}>
         <Avatar />
         <p className={styles.profile_header__name}>name</p>
-        <p className={styles.profile_header__job}>job</p>
-        <p className={styles.profile_header__rank}>rank</p>
-        <p className={styles.profile_header__urls}>url icons</p>
+        <p className={styles.profile_header__job}>[ JOB ]</p>
+        <p className={styles.profile_header__rank}>RANK</p>
+        <div className={[styles.profile_header__urls, "flex w-30 my-3"].join(' ')}>
+          {/* TODO : url list with api */}
+          {['url_default', 'url_git', 'url_home'].map(url => (
+            <div className={styles.icons} key={url}>
+              <Image
+                src={`/images/svg/${url}.svg`}
+                alt={url}
+                width={20}
+                height={20}
+              />
+            </div>
+          ))}
+        </div>
         <p className={styles.profile_header__comment}>
           {comment || '한 줄 소개'}
         </p>
