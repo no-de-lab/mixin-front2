97f7d683a68c7c8e6f0434b4fa134352d56b62e5
cathyleeu <leeyu823@gmail.com>
2021-04-24 01:58:26 +0900

    feat : google login on right modal

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import GoogleLoginButton from './Auth/GoogleLoginButton';
+import GoogleLoginButton from './GoogleLoginButton';
 import { observer } from "mobx-react-lite"
-import { useStore } from '../modules';
-
+import { useStore } from '../../modules';
+import styles from './Auth.module.scss';
 
 export default observer(function Login(props) {
   const {authStore} = useStore();
+
   async function onSuccess(res, provider) {
     const [success, err] = await authStore.login({ provider, accessToken: res.tokenId});
-
     if (!success) {
       alert(err);
       return;
     }
     authStore.setAuth(res.profileObj);
-
   }
-  
-  function onFailure() {
-    alert('fail to login');
-  };
   return (
-    <>
-      <GoogleLoginButton onSuccess={(res) => onSuccess(res, 'GOOGLE')} onFailure={onFailure} />
-    </>
+    <div className={styles.login__container}>
+      <p className={styles.login__header}>Register / Login</p>
+      <GoogleLoginButton onSuccess={(res) => onSuccess(res, 'GOOGLE')} />
+    </div>
   )
 })
