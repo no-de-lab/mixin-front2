dd2125dc2ee13418d41f692f972c76b045a818ae
GeekyCathy <leeyu823@gmail.com>
2021-06-11 11:24:52 +0900

    feat : reload after login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import GoogleLoginButton from './GoogleLoginButton';
-import { observer } from "mobx-react-lite"
+import { observer } from "mobx-react-lite";
+import { useRouter } from 'next/router';
 import { useStore } from '../../modules';
 import styles from './Auth.module.scss';
 
 export default observer(function Login(props) {
   const {authStore} = useStore();
-
+  const router = useRouter();
   async function onSuccess(res, provider) {
     const [success, err] = await authStore.login({ provider, res});
+    router.reload();
     if (!success) {
       alert(err);
       return;
