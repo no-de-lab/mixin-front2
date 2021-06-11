6df0a1037e1ebb5147e2c0e6311f58aca91d0727
GeekyCathy <leeyu823@gmail.com>
2021-06-11 14:59:25 +0900

    style : reuse crawl style

--- src/pages/mypage/body/bookmark.js
+++ src/pages/mypage/body/bookmark.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import HomeCrawlItems from '../../../layout/crawling/HomeCrawItems';
+import styles from '../../../layout/crawling/HomeLayout.module.scss'
 
 function Bookmark(props) {
   const {articles} = props;
   return (
-    <HomeCrawlItems articles={articles} />
+    <div className={styles.crawl__box}>
+      <HomeCrawlItems articles={articles} />
+    </div>
   );
 };
 
