5e8bf58a7495995139b16c2147d4e902f4f10241
GeekyCathy <leeyu823@gmail.com>
2021-06-17 02:49:47 +0900

    fix : inline style

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function Custom404() {
-  const inlineStyle = {
-    'font-family': "'Blender Pro', sans-serif",
-  };
   return (
     <>
       <Head>
         <title>Mix in | Not Found</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="w-screen text-center flex flex-col justify-center" style={inlineStyle}>
+      <div className="w-screen text-center flex flex-col justify-center" style={{ fontFamily: "'Blender Pro', sans-serif" }}>
         <h1 className="text-9xl">404</h1>
         <h1 className="text-4xl">Page Not Found</h1>
       </div>
